c2d9835ffb5218963e079f0824b8c58938690268
jimmy <su.jz@outlook.com>
2021-04-04 11:33:23 +0800

    TEST SCRIPT

--- projects/face_detection/test.py
+++ projects/face_detection/test.py
@@ -1,45 +1,75 @@
-import argparse
+#! /usr/bin/env python
 import torch
 import cv2
+import numpy as np
 import tqdm
 from pathlib import Path
-
-from xvision.model import fd as MODELS, initialize_model
 from xvision.utils import Saver
-from xvision.ops import BBoxAnchors
+from xvision.models.fd import Slim
+from xvision.ops.anchors import BBoxAnchors
+from xvision.transforms.warp import warp_affine
+from xvision.transforms import matrix2d
+from xvision.models.fd.predictor import Predictor
+from xvision.transforms.boxes import bbox2rect, bbox_affine
+from xvision.utils.draw import draw_bbox, draw_points
 
 
 def main(args):
-    model_args = args.test.model.args
-    model_kwargs = args.test.model.kwargs
-    model_name = args.test.model.name
-    model = initialize_model(MODELS, model_name, model_args, model_kwargs)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    if args.device is not None:
-        device = torch.device(torch.device)
-
-    state = Saver.load_best_from_folder(args.workdir, map_location='cpu')
-    model.load_state_dict(state['model'])
-
-    model = model.to(device)
-    model.eval()
-
+    workdir = Path(args.workdir)
+    workdir.mkdir(parents=True, exist_ok=True)
+    model = Slim(phase='test')  # FIXME:  hard coded
     prior = BBoxAnchors(args.dsize, args.strides, args.fsizes, args.layouts,
                         args.iou_threshold, args.encode_mean, args.encode_std)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    state = Saver.load_best_from_folder(workdir, map_location='cpu')
+    model.load_state_dict(state['model'])
+    predictor = Predictor(
+        model, prior, args.test.score_threshold, args.test.iou_threshold, device)
+    output_dir = workdir / 'result'
+    image_dir = Path(args.test.image_dir)
+    for imagefile in tqdm.tqdm(image_dir.rglob('*.jpg')):
+        relpath = imagefile.relative_to(image_dir)
+        outpath = output_dir / relpath
+        outpath.parent.mkdir(parents=True, exist_ok=True)
+        image = cv2.imread(str(imagefile), cv2.IMREAD_COLOR)
+        scale = (args.test.long_size / np.array(image.shape[:2])).min()
+        w, h = (np.array(image.shape[:2]) * scale).astype(np.int32)[::-1]
 
-    for path in tqdm.tqdm(args.test.image_dir.rglob(f'*.jpg')):
-        image = cv2.imread(str(path), cv2.IMREAD_COLOR)
+        matrix = matrix2d.scale(scale)
+        warped = warp_affine(image, matrix, (w, h))
+        score, boxes, points = predictor.predict(warped)
 
-        # resize image with short size as config
-        h, w = image.shape[:2]
-        scale = max(args.test.short_size / h, args.test.short_size / w)
-        dh, dw = int(scale * h), int(scale * w)
+        n = score.size
+        if n > 0:
+            matrix = np.linalg.inv(matrix)
+            boxes = bbox_affine(boxes, matrix)
+            # k, p, 2
+            points = points.reshape(-1, 2) @ matrix[:2, :2].T + matrix[:2, 2]
+            points = points.reshape(score.size, -1, 2)
 
-        
+            # draw result
+            draw_bbox(image, boxes)
+            draw_points(image, points)
+
+        cv2.imwrite(str(outpath), image)
+
+        # write output txt
+        resfile = outpath.with_suffix('.txt')
+        with open(resfile, 'wt') as f:
+            f.write('{}\n'.format(imagefile.name))
+            f.write(f'{n}\n')
+            if n > 0:
+                boxes = bbox2rect(boxes)
+                for score, box in zip(score.tolist(), boxes):
+                    box = box.reshape(-1).tolist()
+                    line = ' '.join(str(v) for v in box)
+                    f.write(f'{line} {score}\n')
+
 
 if __name__ == '__main__':
     from config import cfg
+
     cfg.parse_args()
     cfg.freeze()
+
     main(cfg)
