7cc0234574e17ed000fadacebf427d544c297b9b
jimmy <su.jz@outlook.com>
2021-04-05 14:35:58 +0800

    voc wrapper

--- xvision/datasets/voc.py
+++ xvision/datasets/voc.py
@@ -1,9 +1,37 @@
-from torchvision.datasets import VOCDetection, voc
+import cv2
+import xml.etree.ElementTree as ET
+from torchvision.datasets import VOCDetection as _Voc
+from typing import Optional, Callable, Tuple, Any
+
+
+class VOCDetection(_Voc):
+    """VOC detection dataset that using cv2 backend
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is a dictionary of the XML tree.
+        """
+        img = cv2.imread(self.images[index], cv2.IMREAD_COLOR)
+        target = self.parse_voc_xml(
+            ET.parse(self.annotations[index]).getroot())
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
 
 
 
 if __name__ == '__main__':
-    data = VOCDetection('./workspace/voc/', download=True)
+    data = VOCDetection('/Users/jimmy/Documents/data/', download=False)
 
     for img, ann in data:
-        print(ann)
+        cv2.imshow("img", img)
+        cv2.waitKey()
