ab62db88f2d372e64daa1d7c4bdbddf479b3a788
jimmy <su.jz@outlook.com>
2021-04-07 01:57:48 +0800

    update

--- test/test_mmap.py
+++ test/test_mmap.py
@@ -1,4 +1,4 @@
-from xvision.datasets.mmap import structured_dtype, create_mmap_dataset
+from xvision.datasets.mmap import _structured_dtype, create_mmap_dataset, MMap
 import numpy as np
 import cv2
 
@@ -8,18 +8,24 @@ from xvision.datasets.wflw import WFLW
 def transform(item):
     image = cv2.imread(str(item['path']))
     image =cv2.resize(image, (128, 128))
-    return {
-        'image': image,
-        'shape': item['shape']
-    }
+    item.pop('path')
+    return image, item
 
 label = '/Users/jimmy/Documents/Data/WFLW/WFLW_annotations/list_98pt_rect_attr_train_test/list_98pt_rect_attr_test.txt'
 images = '/Users/jimmy/Documents/Data/WFLW/WFLW_images'
 gen = WFLW.parse(label, images)
-fp = create_mmap_dataset('mmap.npy', gen, transform, 8)
+fp = MMap.create('tuple.npy', gen, transform)
 
+fp = MMap('tuple.npy')
 
-for v in fp:
-    image = v['image']
+for (image, item ), origin in zip(fp,WFLW.parse(label, images)) :
+    # image = v['image']
+    item.pop('shape')
+    item.pop('bbox')
+    origin.pop('path')
+    origin.pop('bbox')
+    origin.pop('shape')
+    print(item)
+    print(origin)
     cv2.imshow("v", image)
     cv2.waitKey()
--- xvision/datasets/mmap.py
+++ xvision/datasets/mmap.py
@@ -6,27 +6,27 @@ from torch.utils.data import Dataset
 from joblib import Parallel, delayed
 
 
-def structured_dtype(value):
+def _structured_dtype(value):
     if isinstance(value, tuple):
         # f0, f1...
-        return np.dtype([('', structured_dtype(v)) for v in value])
+        return np.dtype([('', _structured_dtype(v)) for v in value])
     elif isinstance(value, dict):
-        return np.dtype([(k, structured_dtype(v)) for k, v in value.items()])
+        return np.dtype([(k, _structured_dtype(v)) for k, v in value.items()])
     elif isinstance(value, np.ndarray):
         return np.dtype((value.dtype, value.shape))
     elif isinstance(value, Number):
         return np.dtype(type(value))
     else:
-        raise ValueError(f'Unsupport structured type: {value}')
+        raise ValueError(f'Unsupport structured type with value: {value}')
 
 
-def structured_assign(dst, key, value):
+def _structured_assign(dst, key, value):
     if isinstance(value, (tuple, list)):
         for i, v in enumerate(value):
-            structured_assign(dst[key], f'f{i}', v)
+            _structured_assign(dst[key], f'f{i}', v)
     elif isinstance(value, dict):
         for k, v in value.items():
-            structured_assign(dst[key], k, v)
+            _structured_assign(dst[key], k, v)
     elif isinstance(value, np.ndarray):
         dst[key][...] = value
     else:
@@ -40,13 +40,13 @@ def _is_tuple_fileds(fileds):
     return True
 
 
-def structured_unfold(value):
+def _structured_unfold(value):
     dtype = value.dtype
     if dtype.fields:
         if _is_tuple_fileds(dtype.fields):
-            return tuple([structured_unfold(value[k]) for k in dtype.fields.keys()])
+            return tuple([_structured_unfold(value[k]) for k in dtype.fields.keys()])
         else:
-            return {k: structured_unfold(value[k]) for k in dtype.fields.keys()}
+            return {k: _structured_unfold(value[k]) for k in dtype.fields.keys()}
     else:
         return value
 
@@ -55,14 +55,14 @@ def create_mmap_dataset(filename, data, transform, num_workers=1):
     if isinstance(data, GeneratorType):
         data = list(data)
     item = transform(data[0].copy())
-    data_type = structured_dtype(item)
+    data_type = _structured_dtype(item)
     shape = (len(data),)
     fp = np.lib.format.open_memmap(
         filename, mode='w+', dtype=data_type, shape=shape)
 
     def process(i, v):
         v = transform(v)
-        structured_assign(fp, i, v)
+        _structured_assign(fp, i, v)
 
     if num_workers > 1:
         Parallel(n_jobs=num_workers)(delayed(process)(i, v)
@@ -70,7 +70,7 @@ def create_mmap_dataset(filename, data, transform, num_workers=1):
     else:
         for i, v in enumerate(tqdm.tqdm(data, desc=f'memmaping to {filename}')):
             v = transform(v)
-            structured_assign(fp, i, v)
+            _structured_assign(fp, i, v)
     fp.flush()
     return np.lib.format.open_memmap(filename, mode='r')
 
@@ -89,10 +89,13 @@ class MMap(Dataset):
 
     def __getitem__(self, index):
         item = self.mmap[index].copy()
-        item = structured_unfold(item)
+        item = _structured_unfold(item)
         if self.transform:
             item = self.transform(item)
         return item
+    
+    create = create_mmap_dataset
+
 
 
 if __name__ == '__main__':
