ee899f719ed222e403ce52134b2ad7210f18c2f9
jimmy <su.jz@outlook.com>
2021-04-06 21:22:27 +0800

    save training config

--- projects/fa/config.py
+++ projects/fa/config.py
@@ -9,11 +9,11 @@ _C.padding = 0.2
 _C.lr = 0.2
 _C.momentum = 0.9
 _C.weight_decay = 5e-4
-_C.batch_size = 16
+_C.batch_size = 512
 _C.start_step = 0       # for resume
-_C.total_steps = 10000   # training steps
-_C.num_workers = 8
-_C.eval_interval = 5    # evaluate interval
+_C.total_steps = 100000   # training steps
+_C.num_workers = 6
+_C.eval_interval = 100    # evaluate interval
 _C.left_eye = [66, 70]
 _C.right_eye = [75, 79]
 
@@ -23,7 +23,7 @@ _C.model.name = 'mbv2'
 
 _C.wflw = 'path/to/wflw/data'
 _C.ibug = 'path/to/ibug/data'
-_C.jdlmk = '/Users/jimmy/Documents/data/FA/FLL2/'
+_C.jdlmk = '/dockerdata/train'
 
 _C.augments = CN()
 _C.augments.rotate = 15
