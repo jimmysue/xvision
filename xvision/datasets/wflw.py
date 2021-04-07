f441f94e57b8fe32cdb7e581c33d3fbc95dd2689
jimmy <su.jz@outlook.com>
2021-04-08 00:21:14 +0800

    add symmetry attr

--- xvision/datasets/wflw.py
+++ xvision/datasets/wflw.py
@@ -7,6 +7,13 @@ from pathlib import Path
 class WFLW(Dataset):
     """WFLW dataset
     """
+    __symmetry__ = [32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18,
+                    17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
+                    0, 46, 45, 44, 43, 42, 50, 49, 48, 47, 37, 36, 35, 34, 33, 41,
+                    40, 39, 38, 51, 52, 53, 54, 59, 58, 57, 56, 55, 72, 71, 70, 69,
+                    68, 75, 74, 73, 64, 63, 62, 61, 60, 67, 66, 65, 82, 81, 80, 79,
+                    78, 77, 76, 87, 86, 85, 84, 83, 92, 91, 90, 89, 88, 95, 94, 93,
+                    97, 96]
 
     def __init__(self, label_file, image_dir, transform=None) -> None:
         super().__init__()
@@ -37,7 +44,8 @@ class WFLW(Dataset):
             for line in f:
                 items = line.strip().split()
                 shape = np.array(items[:196]).astype(np.float32).reshape(-1, 2)
-                bbox = np.array(items[196:196+4]).astype(np.float32).reshape(-1)
+                bbox = np.array(
+                    items[196:196+4]).astype(np.float32).reshape(-1)
                 attr = [int(v) for v in items[200:200+6]]
                 keys = ['pose', 'expression', 'illumination',
                         'makeup', 'occlusion', 'blur']
@@ -54,13 +62,15 @@ if __name__ == '__main__':
     label = '/Users/jimmy/Documents/Data/WFLW/WFLW_annotations/list_98pt_rect_attr_train_test/list_98pt_rect_attr_train.txt'
     image = '/Users/jimmy/Documents/Data/WFLW/WFLW_images'
     data = WFLW(label, image)
+
+    # calc meanshape
+
     shapes = data.shapes
     print(shapes.shape)
     for item in data:
         image = item.pop('image')
         shape = item.pop('shape')
-        draw_points(image, shape, radius=5)
-        print(item)
+        draw_points(image, shape, radius=2, plot_index=True)
         cv2.imshow('v', image)
         k = cv2.waitKey()
         if k == ord('q'):
