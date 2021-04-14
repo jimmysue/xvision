c1e2c4ed9045cfeddba768cbae29a4b7b2931175
jimmy <su.jz@outlook.com>
2021-04-14 23:09:21 +0800

    add docs

--- xvision/ops/emd_loss.py
+++ xvision/ops/emd_loss.py
@@ -2,6 +2,22 @@ import torch
 
 
 def emd_loss(preds, targets, p=2, reduction='mean'):
+    '''Impolement Earth Mover's Distance loss
+
+    This loss function was proposed in paper `NIMA: Neural Image Assessment`_
+
+    Args:
+        preds (Tensor): batched predictions of the probability mass function.
+            The shape should be [N, C]
+        targets (Tensor): target probability mass function same shape with preds
+        p (int, optional): order of p-norm. Defaults to 2.
+        reduction (str, optional): reduction method which can be one of 'mean', 'sum' 
+            or none. Defaults to 'mean'.
+
+    Returns:
+        [Tensor]: the loss
+    .. _NIMA\: Neural Image Assessment: http://arxiv.org/abs/1709.05424
+    '''
     cdf_y = torch.cumsum(targets, dim=1)
     cdf_pred = torch.cumsum(preds, dim=1)
     cdf_diff = cdf_pred - cdf_y
