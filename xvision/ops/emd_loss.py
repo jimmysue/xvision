fix emd loss calculation

xvision/ops/emd_loss.py
@@ -1,10 +1,22 @@
 import torch
 
 
-def emd_loss(preds, targets):
+def emd_loss(preds, targets, p=2, reduction='mean'):
     cdf_y = torch.cumsum(targets, dim=1)
     cdf_pred = torch.cumsum(preds, dim=1)
     cdf_diff = cdf_pred - cdf_y
-    emd_loss = torch.sqrt(torch.mean(torch.pow(torch.abs(cdf_diff), 2)))
-    return emd_loss.mean()
+    den = cdf_diff.size(-1) ** (1/p)
+    loss = torch.norm(cdf_diff, p=p, dim=-1) / den
+    if reduction == 'mean':
+        loss = loss.mean()
+    elif reduction == 'sum':
+        loss = loss.sum()
+    return loss
 
+
+if __name__ == '__main__':
+    a = torch.rand(128, 10)
+    b = torch.rand(128, 10)
+
+    loss = emd_loss(a, b)
+    print(loss)